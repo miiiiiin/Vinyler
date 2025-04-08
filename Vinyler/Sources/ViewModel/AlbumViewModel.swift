139a93fb5a09d12be2a55fa126a0a1018ecb3c69
songkyung <min.songkyung@gmail.com>
2025-04-09 00:04:34 +0900

    refactor: AlbumVC binding 리팩토링

--- Vinyler/Sources/ViewModel/AlbumViewModel.swift
+++ Vinyler/Sources/ViewModel/AlbumViewModel.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import RxSwift
+import RxCocoa
 import Action
 import Toaster
 
@@ -18,6 +19,7 @@ protocol AlbumViewModelInput {
 protocol AlbumViewModelOutput {
     var releaseInfo: Observable<Release> { get }
     var isLike: Observable<Bool> { get }
+    var albumImage: Driver<UIImage?> { get }
 }
 
 protocol AlbumViewModelType {
@@ -56,6 +58,7 @@ class AlbumViewModel: AlbumViewModelInput, AlbumViewModelOutput, AlbumViewModelT
     
     var releaseInfo: Observable<Release>
     var isLike: Observable<Bool> = .just(false)
+    var albumImage: Driver<UIImage?>
     
     
     // MARK: - Private -
@@ -66,8 +69,26 @@ class AlbumViewModel: AlbumViewModelInput, AlbumViewModelOutput, AlbumViewModelT
     init(sceneCoordinator: SceneCoordinatorType, useCase: VinylUseCase, release: Release) {
         self.sceneCoordinator = sceneCoordinator
         self.useCase = useCase
-        self.releaseInfo = Observable.just(release);
+        self.releaseInfo = Observable.just(release)
         
-        
+        setAlbumImage()
+    }
+    
+    private func setAlbumImage() {
+        self.albumImage = self.releaseInfo
+            .map { release -> URL? in
+                let primaryImage = release.images.filter { $0.type == .primary }.first
+                let anyImage = release.images.first
+                return URL(string: (primaryImage ?? anyImage)?.resourceUrl ?? "")
+            }
+            .flatMapLatest { imageURL -> Driver<UIImage?> in
+                guard let url = imageURL else {
+                    return Driver.just(nil)
+                }
+                let request = URLRequest(url: imageURL)
+                return URLSession.shared.rx.data(request: request)
+                    .map(UIImage.init)
+                    .asDriver(onErrorJustReturn: nil)
+            }
     }
 }
