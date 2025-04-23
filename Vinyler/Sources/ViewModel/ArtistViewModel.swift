cae1d9215e490ff8ed02d0d14b518534542370fd
songkyung <min.songkyung@gmail.com>
2025-04-23 23:28:10 +0900

    feature: artistViewModel 데이터 추가

--- Vinyler/Sources/ViewModel/ArtistViewModel.swift
+++ Vinyler/Sources/ViewModel/ArtistViewModel.swift
@@ -17,6 +17,10 @@ protocol ArtistViewModelInput {
 }
 
 protocol ArtistViewModelOutput {
+    var artistInfo: Observable<Artist> { get }
+    var members: Observable<[ArtistDetail]> { get }
+    var profile: Observable<String> { get }
+    var image: Driver<UIImage?> { get }
 }
 
 protocol ArtistViewModelType {
@@ -33,11 +37,15 @@ class ArtistViewModel: ArtistViewModelInput, ArtistViewModelOutput, ArtistViewMo
     
     lazy var dismissAction: CocoaAction = {
         CocoaAction { [unowned self] _ in
-            return self.sceneCoordinator.dismissAll(animated: true).asObservable().map { _ in }
+            return self.sceneCoordinator.dismiss(animated: true).asObservable().map { _ in }
         }
     }()
     
-
+    var artistInfo: Observable<Artist>
+    var members: Observable<[ArtistDetail]>
+    var profile: Observable<String>
+    var image: Driver<UIImage?> = Driver.just(nil)
+    
     // MARK: - Private -
     
     private let sceneCoordinator: SceneCoordinatorType
@@ -46,7 +54,29 @@ class ArtistViewModel: ArtistViewModelInput, ArtistViewModelOutput, ArtistViewMo
     init(sceneCoordinator: SceneCoordinatorType, useCase: VinylUseCase, artistInfo: Artist) {
         self.sceneCoordinator = sceneCoordinator
         self.useCase = useCase
+        self.artistInfo = .just(artistInfo)
+        self.members = .just(artistInfo.members)
+        self.profile = .just(artistInfo.profilePlaintext)
+        self.setImage(images: artistInfo.images)
+    }
     
+    private func setImage(images: [Image]) {
+        self.image = self.images
+            .asDriver(onErrorDriveWith: .empty())
+            .map { images -> URL? in
+                let primaryImage = images.first(where: { $0.type == .primary} )
+                let anyImage = images.first
+                return URL(string: (primaryImage ?? anyImage)?.resourceUrl ?? "")
+            }
+            .flatMapLatest { imageUrl -> Driver<UIImage?> in
+                guard let url = imageUrl else {
+                    return Driver.just(nil)
+                }
+                let request = URLRequest(url: url)
+                return URLSession.shared.rx.data(request: request)
+                    .map(UIImage.init)
+                    .asDriver(onErrorJustReturn: nil)
+            }
     }
 
 }
